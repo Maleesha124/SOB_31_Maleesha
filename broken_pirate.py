a25caa1301426df42ab164e8cb38a27f5f45bb53
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-28 11:08:39 +0400

    debugged broken_pirte

---
+++ broken_pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?") #added the closing double quotes
+if greeting in ["Arrr!"]:   #corrected the parantheses to square brackets
+    print("Go away, pirate.") #indentation
+else :  #added colon and changed the condition to else
+    print("Greetings, hater of pirates!") #indentation 
